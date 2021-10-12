4479151bf331d1d7050e3ae6292a1793d30542ea
LeeSangMin1029 <sangmins39@gmail.com>
2021-10-12 13:25:30 +0900

    전체 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+import Header from './Components/Header.js';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding : 0px;
+    margin: 0px;
+    box-sizing : border-box;
+  }
+  *{
+    padding: 0px;
+    margin: 0px;
+  }
+`;
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
