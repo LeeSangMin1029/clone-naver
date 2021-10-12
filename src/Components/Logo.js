1ec8790560a83786aacb443731933974a8166c99
LeeSangMin1029 <sangmins39@gmail.com>
2021-10-12 13:55:49 +0900

    objectFit에서 objectfit으로 변경

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -7,7 +7,7 @@ const Link = styled.a`
 const Logo = () => {
   return (
     <Link href="/">
-      <img alt="logo" src="/img/logo_green.png" objectFit="fill" layout="fill" quality={100} width={165} height={32} />
+      <img alt="logo" src="/img/logo_green.png" objectfit="fill" layout="fill" quality={100} width={165} height={32} />
     </Link>
   );
 };
