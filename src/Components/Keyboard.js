46fa2589a664754df0c7b223c615b4b55c147bc6
LeeSangMin1029 <sangmins39@gmail.com>
2021-10-13 18:03:15 +0900

    키보드 버튼 추가

---
+++ src/Components/Keyboard.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { ReactComponent as KeyboardImage } from '../svg/keyboard.svg';
+
+const KeyboardStyle = styled.div`
+  margin-right: 12px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const Keyboard = () => {
+  return (
+    <KeyboardStyle>
+      <KeyboardImage fill="#CCD1D1" width="25px" height="15px" />
+    </KeyboardStyle>
+  );
+};
+
+export default Keyboard;
---
+++ src/Components/SearchForm.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import Keyboard from './Keyboard.js';
+
+const SearchStyle = styled.div`
+  width: 600px;
+  height: 52px;
+  border: 2px solid #19ce60;
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  height: 100%;
+  outline: 0;
+  font-size: 18px;
+  margin: 0 12px;
+  flex: 1;
+`;
+
+const SearchForm = () => {
+  return (
+    <SearchStyle>
+      <form action="/" method="get" name="serach" />
+      <Input type="text" form="search" id="header-search" name="search" />
+      <Keyboard />
+    </SearchStyle>
+  );
+};
+
+export default SearchForm;
