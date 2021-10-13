0b45ec4cf099e6bd3e4e4814d121dae31fdfe625
LeeSangMin1029 <sangmins39@gmail.com>
2021-10-14 00:27:46 +0900

    필요없는 속성 제거

--- src/Components/ArrowToSetStartingPage.js
+++ src/Components/ArrowToSetStartingPage.js
@@ -4,7 +4,6 @@ import { ReactComponent as ArrowRight } from '../svg/arrow-right.svg';
 const ArrowStyle = styled(ArrowRight)`
   margin: 0 5px;
   box-shadow: 0px 2px 3px 1px #d0d3d9;
-  border: none;
   border-radius: 100%;
 `;
 
