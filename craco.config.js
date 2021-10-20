023443d4ce6f7a7d09180c126c8e9e3d522a5bd1
LeeSangMin1029 <sangmins39@gmail.com>
2021-10-21 04:36:31 +0900

    craco 적용

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+const CracoAlias = require('craco-alias');
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: { source: 'jsconfig', jsConfigPath: 'jsconfig.paths.json' },
+    },
+  ],
+};
