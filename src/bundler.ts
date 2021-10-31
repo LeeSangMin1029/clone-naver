a028504d821e8552d4db76bbcb3499ca5e970315
LeeSangMin1029 <sangmins39@gmail.com>
2021-10-31 19:28:06 +0900

    경로 변경

--- src/bundler.ts
+++ src/bundler.ts
@@ -1,7 +1,7 @@
 import { exec } from 'https://deno.land/x/exec@0.0.5/mod.ts';
-const configFile = '--config ./deno.json';
-const importMap = '--importmap=import_map.json ';
-const inSrc = './src/components/';
-const outSrc = './public/js/';
+const configFile = '--config ../deno.json';
+const importMap = '--importmap=../import_map.json ';
+const inSrc = './components/';
+const outSrc = '../public/js/';
 const command = `deno bundle ${configFile} ${importMap} ${inSrc}client.tsx ${outSrc}client.js`;
 await exec(command);
