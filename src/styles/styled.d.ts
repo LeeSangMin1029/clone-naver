ad2079e5d3c9d19cacc5aff5db5eb62398b0549e
LeeSangMin1029 <sangmins39@gmail.com>
2021-10-31 20:50:14 +0900

    theme 타입 추가

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,5 +3,22 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     body: string;
+    header: string;
+    bg_startLink: string;
+    startLinkShadow: string;
+    startLinkColor: string;
+    borderBottom: string;
+    toolbar: string;
+    toolbarBackground: string;
+    toolbarShadow: string;
+    inputColor: string;
+    menuWordColor: string;
+    themeContent: string;
+    bg_themeBtn: string;
+    bg_themeBtnHover: string;
+    themeBtnImg: string;
+    themeBtnWordHover: string;
+    themeBtnWord: string;
+    themeBtnImgHover: string;
   }
 }
