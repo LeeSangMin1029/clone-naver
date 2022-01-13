1b0944a6e8ba1f529405a34cf89f5ea64fd23899
LeeSangMin1029 <sangmins39@gmail.com>
2022-01-14 01:49:01 +0900

    유틸 모듈 추가
    - 재사용가능한 css 모듈 추가

---
+++ src/utils/mod.ts
@@ -0,0 +1,13 @@
+import { css } from 'styled-components';
+
+const fontFamilyStyle = css`
+  font-family: 'Font Awesome 5 Free';
+  font-weight: 900;
+  display: inline-block;
+  font-style: normal;
+  font-variant: normal;
+  text-rendering: auto;
+  -webkit-font-smoothing: antialiased;
+`;
+
+export { fontFamilyStyle };
